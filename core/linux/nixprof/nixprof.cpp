53f8862e3a9059e09537d74d8d593fce9f8ff108
Chen Yang <adamyoung1234@outlook.com>
2021-06-01 19:07:09 -0400

    Fixed ambigous name under macOS

--- core/linux/nixprof/nixprof.cpp
+++ core/linux/nixprof/nixprof.cpp
@@ -50,7 +50,7 @@
 
 static int tick_count=0;
 static pthread_t proft;
-static pthread_t thread[2];
+static pthread_t threads[2];
 static void*     prof_address[2];
 static u32 prof_wait;
 
@@ -69,8 +69,8 @@ void prof_handler (int sn, siginfo_t * si, void *ctxr)
 	context_from_segfault(&ctx, ctxr);
 	
 	int thd=-1;
-	if (pthread_self()==thread[0]) thd=0;
-	else if (pthread_self()==thread[1]) thd=1;
+	if (pthread_self()==threads[0]) thd=0;
+	else if (pthread_self()==threads[1]) thd=1;
 	else return;
 
 	prof_address[thd] = (void*)ctx.pc;
@@ -87,7 +87,7 @@ void install_prof_handler(int id)
 	act.sa_flags = SA_SIGINFO | SA_RESTART;
 	sigaction(SIGPROF, &act, &segv_oact);
 
-	thread[id]=pthread_self();
+	threads[id]=pthread_self();
 }
 
 static void prof_head(FILE* out, const char* type, const char* name)
@@ -294,7 +294,7 @@ static void* profiler_main(void *ptr)
 		{
 			tick_count++;
 			// printf("Sending SIGPROF %08X %08X\n",thread[0],thread[1]);
-			for (int i = 0; i < 2; i++) pthread_kill(thread[i], SIGPROF);
+			for (int i = 0; i < 2; i++) pthread_kill(threads[i], SIGPROF);
 			// printf("Sent SIGPROF\n");
 			usleep(prof_wait);
 			// fwrite(&prof_address[0],1,sizeof(prof_address[0])*2,prof_out);
